4a73ca0ab74209855790d4dde19e446a605f6c48
Jeda777 <jakub.jeda@wp.pl>
2023-12-14 22:56:52 +0100

    Prisma setup
    Setup prisma, prisma models, connected to AWS MySQL database and setup profile creation id db.

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,9 @@
 import ThemeButton from '@/components/UI/ThemeButton'
+import { setupProfile } from '@/lib/setupProfile'
 import { UserButton } from '@clerk/nextjs'
 
-const Home = () => {
+const Home = async () => {
+  const profile = await setupProfile()
   return (
     <div>
       <UserButton />

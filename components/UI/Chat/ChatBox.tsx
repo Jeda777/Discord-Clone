326089f658bc0a2579044567ee1696564d003a25
Jeda777 <jakub.jeda@wp.pl>
2024-01-22 23:24:51 +0100

    Created messages box

--- components/UI/Chat/ChatBox.tsx
+++ components/UI/Chat/ChatBox.tsx
@@ -1,9 +1,11 @@
 import ChatHeader from './ChatHeader'
+import MessagesBox from './MessagesBox'
 
 const ChatBox = ({ type, name, userImageUrl }: { type: 'channel' | 'conversation'; name: string; userImageUrl?: string }) => {
   return (
     <div className='w-full h-full'>
       <ChatHeader name={name} type={type} userImageUrl={userImageUrl} />
+      <MessagesBox />
     </div>
   )
 }
---
+++ components/UI/Chat/MessagesBox.tsx
@@ -0,0 +1,5 @@
+const MessagesBox = () => {
+  return <div className='flex-1'>MessagesBox</div>
+}
+
+export default MessagesBox

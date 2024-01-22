a2289f7abfe27045b9149ae3e4d8d9cb91b3a902
Jeda777 <jakub.jeda@wp.pl>
2024-01-22 23:52:20 +0100

    Created chat input

--- components/UI/Chat/ChatBox.tsx
+++ components/UI/Chat/ChatBox.tsx
@@ -1,11 +1,13 @@
 import ChatHeader from './ChatHeader'
+import ChatInput from './ChatInput'
 import MessagesBox from './MessagesBox'
 
 const ChatBox = ({ type, name, userImageUrl }: { type: 'channel' | 'conversation'; name: string; userImageUrl?: string }) => {
   return (
-    <div className='w-full h-full'>
+    <div className='w-full h-full flex flex-col'>
       <ChatHeader name={name} type={type} userImageUrl={userImageUrl} />
       <MessagesBox />
+      <ChatInput />
     </div>
   )
 }
---
+++ components/UI/Chat/ChatInput.tsx
@@ -0,0 +1,21 @@
+'use client'
+
+import { IoAdd } from 'react-icons/io5'
+
+const ChatInput = () => {
+  return (
+    <div className='w-full p-2'>
+      <form className='w-full flex gap-2 bg-secondary p-2 rounded-2xl items-center'>
+        <button type='button'>
+          <IoAdd className='text-3xl text-primary' />
+        </button>
+        <input
+          className='input-block focus:outline-none w-full bg-transparent border-none text-primary text-sm'
+          placeholder='Send a message...'
+        />
+      </form>
+    </div>
+  )
+}
+
+export default ChatInput

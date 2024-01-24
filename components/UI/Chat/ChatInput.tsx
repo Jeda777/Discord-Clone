769d72c7c7dc8ff1c6646bb452cd044b865fed0d
Jeda777 <jakub.jeda@wp.pl>
2024-01-24 22:13:40 +0100

    Update ChatInput.tsx

--- components/UI/Chat/ChatInput.tsx
+++ components/UI/Chat/ChatInput.tsx
@@ -5,8 +5,8 @@ import { IoAdd } from 'react-icons/io5'
 const ChatInput = () => {
   return (
     <div className='w-full p-2'>
-      <form className='w-full flex gap-2 bg-secondary p-2 rounded-2xl items-center'>
-        <button type='button'>
+      <form className='w-full flex gap-2 bg-secondary p-2 rounded-3xl items-center'>
+        <button type='button' className='bg-muted rounded-full p-1'>
           <IoAdd className='text-3xl text-primary' />
         </button>
         <input

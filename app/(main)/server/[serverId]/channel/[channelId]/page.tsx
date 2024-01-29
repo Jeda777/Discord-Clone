42ef3ee58a6874c5a2b66013244d1aa08c08860d
Jeda777 <jakub.jeda@wp.pl>
2024-01-29 19:50:47 +0100

    Video and audio channels
    Installed and setup livekit

--- app/(main)/server/[serverId]/channel/[channelId]/page.tsx
+++ app/(main)/server/[serverId]/channel/[channelId]/page.tsx
@@ -1,4 +1,5 @@
 import ChatBox from '@/components/UI/Chat/ChatBox'
+import MediaRoom from '@/components/UI/Server/MediaRoom'
 import MembersSideBar from '@/components/UI/Server/MembersSideBar'
 import ServerSidebar from '@/components/UI/Server/ServerSidebar'
 import { currentProfile } from '@/lib/currentProfile'
@@ -42,7 +43,7 @@ const ChannelPage = async ({ params }: { params: { serverId: string; channelId: 
   return (
     <div className='h-full w-full flex'>
       <ServerSidebar server={server} role={role} profileId={profile.id} />
-      {channel.type == ChannelType.TEXT && (
+      {channel.type == ChannelType.TEXT ? (
         <ChatBox
           type='channel'
           name={channel.name}
@@ -51,6 +52,8 @@ const ChannelPage = async ({ params }: { params: { serverId: string; channelId: 
           profileId={profile.id}
           isModerator={isModerator}
         />
+      ) : (
+        <MediaRoom audio={true} video={channel.type === 'VIDEO'} channelId={channel.id} profile={profile} />
       )}
       <MembersSideBar server={server} role={role} profileId={profile.id} />
     </div>
---
+++ components/UI/Server/MediaRoom.tsx
@@ -0,0 +1,88 @@
+'use client'
+
+import { useState, useEffect } from 'react'
+import '@livekit/components-styles'
+import {
+  LiveKitRoom,
+  VideoConference,
+  GridLayout,
+  ParticipantTile,
+  RoomAudioRenderer,
+  ControlBar,
+  useTracks,
+} from '@livekit/components-react'
+import { Track } from 'livekit-client'
+import { Profile } from '@prisma/client'
+
+const MediaRoom = ({
+  channelId,
+  profile,
+  audio,
+  video,
+}: {
+  channelId: string
+  profile: Profile
+  audio: boolean
+  video: boolean
+}) => {
+  const [token, setToken] = useState('')
+
+  useEffect(() => {
+    if (!profile) return
+    ;(async () => {
+      try {
+        const resp = await fetch(`/api/livekit?room=${channelId}&username=${profile.name}`)
+        const data = await resp.json()
+        setToken(data.token)
+      } catch (e) {
+        console.error(e)
+      }
+    })()
+  }, [channelId, profile])
+
+  if (token === '') {
+    return <div className='w-full h-full'>Getting token...</div>
+  }
+
+  return (
+    <LiveKitRoom
+      video={video}
+      audio={audio}
+      token={token}
+      connect={true}
+      serverUrl={process.env.NEXT_PUBLIC_LIVEKIT_URL}
+      // Use the default LiveKit theme for nice styles.
+      data-lk-theme='default'
+      // style={{ height: '100dvh' }}
+    >
+      {/* Your custom component with basic video conferencing functionality. */}
+      <MyVideoConference />
+      {/* The RoomAudioRenderer takes care of room-wide audio for you. */}
+      <RoomAudioRenderer />
+      {/* Controls for the user to start/stop audio, video, and screen 
+        share tracks and to leave the room. */}
+      <ControlBar controls={{ leave: false }} />
+    </LiveKitRoom>
+  )
+}
+
+function MyVideoConference() {
+  // `useTracks` returns all camera and screen share tracks. If a user
+  // joins without a published camera track, a placeholder track is returned.
+  const tracks = useTracks(
+    [
+      { source: Track.Source.Camera, withPlaceholder: true },
+      { source: Track.Source.ScreenShare, withPlaceholder: false },
+    ],
+    { onlySubscribed: false },
+  )
+  return (
+    <GridLayout tracks={tracks} style={{ height: 'calc(100vh - var(--lk-control-bar-height))' }}>
+      {/* The GridLayout accepts zero or one child. The child is used
+        as a template to render all passed in tracks. */}
+      <ParticipantTile />
+    </GridLayout>
+  )
+}
+
+export default MediaRoom
